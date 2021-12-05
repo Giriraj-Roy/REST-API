b818c81816dd109dafac97ad4c409a0f99ba0938
Giriraj Roy <girirajroy2001@gmail.com>
2021-12-05 12:23:17 +0530

    Server.js Setup

---
+++ server.js
@@ -0,0 +1,15 @@
+require('dotenv').config()
+const express = require("express")
+const app = express()
+const mongoose = require("mongoose")
+
+mongoose.connect(process.env.DATABASE_URL//,{
+    //useNewUrlParser: true
+)
+const db = mongoose.connection
+db.on('error', (error) => console.error(error))
+db.once('open', () => console.error('Connected To DataBase'))
+
+app.listen(3000, () => {
+    console.log("server started")
+})
