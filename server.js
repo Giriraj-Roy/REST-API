b85c3a2fe78dc41b62d2d0ac8030943fd2441062
Giriraj Roy <girirajroy2001@gmail.com>
2021-12-28 11:44:05 +0530

    updated server.js

--- server.js
+++ server.js
@@ -3,9 +3,9 @@ const express = require("express")
 const app = express()
 const mongoose = require("mongoose")
 
-mongoose.connect(process.env.DATABASE_URL//,{
-    //useNewUrlParser: true
-)
+mongoose.connect(process.env.DATABASE_URL,{
+    useNewUrlParser: true
+})
 const db = mongoose.connection
 db.on('error', (error) => console.error(error))
 db.once('open', () => console.error('Connected To DataBase'))
